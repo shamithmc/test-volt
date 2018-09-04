89d64d868458a6bdb33cb9108f23ec9a8e4d5812
Shamith Kumar <shamith.kumar@flytxt.com>
2018-09-04 11:43:37 +0530

    updated tests

---
+++ src/test/java/com/flytxt/TestTestMessageBCPrioritization.java
@@ -0,0 +1,13 @@
+package com.flytxt;
+
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+
+public class TestTestMessageBCPrioritization {
+    
+    @Test
+    public void testMessageBCPrioritization(){
+        SpringApplication.run(TestMessageBCPrioritization.class, new String[0]);
+    }
+
+}
